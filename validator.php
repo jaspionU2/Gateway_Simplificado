3580105a790aa0a00d10aa595b0915927c68684c
jaspionU2 <gameraleatorio2202@outlook.com>
2026-01-16 11:48:23 -0300

    fila de validaçõ estatica funcional, utilizando função de callback

--- validator.php
+++ validator.php
@@ -7,42 +7,73 @@ class PaymentValidator
 {
     private array $queueValidation = [];
 
-    public function __construct(){}
+    public function __construct() {}
 
-    public function validate(PaymentDTO $payment)
+    public function enqueueValidation()
     {
-        $this->queueValidation = array ( 
-                [$this, "assertMethodExist"],
-                [$this, "assertPositiveAmount"],
-                [$this, "assertCointExist"],
-                [$this, "assertOrderIdExist"]
-        );  
-
+        $this->queueValidation = [
+            [PaymentValidator::class, 'assertMethodExist'],
+            [PaymentValidator::class, 'assertPositiveAmount'],
+            [PaymentValidator::class, 'assertCoinExist'],
+            [PaymentValidator::class, 'assertOrderIdExist'],
+        ];
         return $this->queueValidation;
     }
 
-    private function assertPositiveAmount(PaymentDTO $payment) : void
+    public function execValidation(PaymentDTO $payment) 
     {
-        if ($payment->paymentAmount <= 0) throw new DomainException("A quantia para pagamento deve ser maior que zero, valor atual é:  {$payment->paymentAmount}");
-
+        foreach($this->queueValidation as $callFunc) {
+            try {
+                call_user_func(array($callFunc[0], $callFunc[1]), $payment);
+            } catch (DomainException $e) {
+                var_dump($e->getMessage());
+            }
+        }
     }
 
-    private function assertOrderIdExist(PaymentDTO $payment) : void
+    private function assertPositiveAmount(PaymentDTO $payment): void
     {
-        if (is_null($payment->idOrder)) throw new DomainException("O identificador de pedido de pagamento não foi informado.");
+        if ($payment->paymentAmount <= 0) {
+            throw new DomainException(
+                "A quantia para pagamento deve ser maior que zero, valor atual é:  {$payment->paymentAmount}",
+            );
+        }
     }
 
-    private function assertCoinExist(PaymentDTO $payment) : void
+    private function assertOrderIdExist(PaymentDTO $payment): void
     {
-        if (is_null($payment->coin)) throw new DomainException("A moeda deve ser informada");
+        if (is_null($payment->idOrder)) {
+            throw new DomainException("O identificador de pedido de pagamento não foi informado.");
+        }
     }
 
-    private function assertMethodExist(PaymentDTO $payment) : void
+    private function assertCoinExist(PaymentDTO $payment): void
+    {
+        if (is_null($payment->coin)) {
+            throw new DomainException("A moeda deve ser informada");
+        }
+    }
+
+    private function assertMethodExist(PaymentDTO $payment): void
     {
         $validMethods = ["pix", "credit_card", "debit_card"];
         $methodIsValid = array_any($validMethods, fn($value) => $payment->paymentMethod == $value);
 
-        if (is_null($payment->paymentMethod) && $methodIsValid) throw new DomainException("O método de pagamento deve ser informado e ser valido.");
+        if (is_null($payment->paymentMethod) || !$methodIsValid) {
+            throw new DomainException("O método de pagamento deve ser informado e ser valido.");
+        }
     }
 }
+$json = json_encode([
+    "paymentAmount" => "105.50",
+    "coin" => "BRL",
+    "idOrder" => "ORD123456",
+    "paymentMethod" => "",
+    "extraLoad" => ["description" => "Produto A"]
+]);
+
+$payment = PaymentDTO::fromJson($json);
+$paymentValidator = new PaymentValidator;
+$paymentValidator->enqueueValidation();
+$paymentValidator->execValidation($payment)
 ?>
